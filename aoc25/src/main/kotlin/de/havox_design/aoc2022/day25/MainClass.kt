3f5396c8df4187adcd26de0a6e13d9963a577220
Gentleman83 (Christian Otto) <Christian@Otto-Uslar.de>
2022-12-02 00:29:53 +0100

    added missing stub for day 25

---
+++ aoc25/src/main/kotlin/de/havox_design/aoc2022/day25/MainClass.kt
@@ -0,0 +1,10 @@
+package de.havox_design.aoc2022.day25
+
+class MainClass {
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            println("please implement me")
+        }
+    }
+}
---
+++ aoc25/src/test/kotlin/de/havox_design/aoc2022/day25/Day25Test.kt
@@ -0,0 +1,11 @@
+package de.havox_design.aoc2022.day25
+
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.fail
+
+class Day25Test {
+    @Test
+    fun test() {
+        fail("implement me")
+    }
+}
