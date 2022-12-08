96ba15adf0347069bd385e6e1ffe6705bcc0aa72
Gentleman83 (Christian Otto) <Christian@Otto-Uslar.de>
2022-12-08 17:40:50 +0100

    Day 8: Increment version

--- aoc08/src/main/kotlin/de/havox_design/aoc2022/day08/TreetopTreeHouse.kt
+++ aoc08/src/main/kotlin/de/havox_design/aoc2022/day08/TreetopTreeHouse.kt
@@ -1,4 +1,10 @@
 package de.havox_design.aoc2022.day08
 
-class TreetopTreeHouse {
+class TreetopTreeHouse(private var filename: String) {
+    fun readFile() {
+        getResourceAsText(filename)
+    }
+
+    private fun getResourceAsText(path: String): List<String> =
+        this.javaClass.classLoader.getResourceAsStream(path)!!.bufferedReader().readLines()
 }
