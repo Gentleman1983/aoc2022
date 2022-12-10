957ca376128018833bee476a8bf879d172d4914d
Gentleman83 (Christian Otto) <Christian@Otto-Uslar.de>
2022-12-10 16:02:17 +0100

    Day 9: Added additional handling for HEAD-AND-TAIL fields

--- aoc09/src/main/kotlin/de/havox_design/aoc2022/day09/Position.kt
+++ aoc09/src/main/kotlin/de/havox_design/aoc2022/day09/Position.kt
@@ -2,7 +2,11 @@ package de.havox_design.aoc2022.day09
 
 data class Position(var knot: Knot? = null, var visitedByTail: Boolean = false) {
     fun visit(knot: Knot) {
-        this.knot = knot
+        this.knot = if (knot == Knot.HEAD && this.knot == Knot.TAIL) {
+            Knot.HEAD_AND_TAIL
+        } else {
+            knot
+        }
 
         if (knot == Knot.TAIL || knot == Knot.HEAD_AND_TAIL) {
             visitedByTail = true
