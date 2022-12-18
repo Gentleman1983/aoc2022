df72625498d24321a7f64d1d5e4ca38c1cf0b764
Gentleman83 (Christian Otto) <Christian@Otto-Uslar.de>
2022-12-18 11:48:28 +0100

    Day 13: Solved task 1

--- aoc13/src/main/kotlin/de/havox_design/aoc2022/day13/DistressSignal.kt
+++ aoc13/src/main/kotlin/de/havox_design/aoc2022/day13/DistressSignal.kt
@@ -1,15 +1,28 @@
 package de.havox_design.aoc2022.day13
 
 class DistressSignal(private var filename: String) {
+    val packetList = readFile()
 
     fun processPart1(): Int =
-        0
+        packetList
+            .chunked(2) { (firstPacket, secondPacket) -> firstPacket <= secondPacket }
+            .withIndex()
+            .filter { currentPacket -> currentPacket.value }
+            .sumOf { currentPacket -> currentPacket.index + 1 }
 
     fun processPart2(): Int =
         0
 
-    private fun readFile() {
+    private fun readFile(): List<Packet> {
+        val fileInput = getResourceAsText(filename)
 
+        return fileInput.mapNotNull { row ->
+            if (row.isNotEmpty()) {
+                Packet.parsePacket(row)
+            } else {
+                null
+            }
+        }
     }
 
     private fun getResourceAsText(path: String): List<String> =
