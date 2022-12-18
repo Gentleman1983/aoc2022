ab9e115f16c8bc28754f364a9c89a1893c9d5635
Gentleman83 (Christian Otto) <Christian@Otto-Uslar.de>
2022-12-18 23:22:02 +0100

    Day 18: Solved part 2

--- aoc18/src/main/kotlin/de/havox_design/aoc2022/day18/BoilingBoulders.kt
+++ aoc18/src/main/kotlin/de/havox_design/aoc2022/day18/BoilingBoulders.kt
@@ -1,22 +1,46 @@
 package de.havox_design.aoc2022.day18
 
 class BoilingBoulders(private var filename: String) {
-    val data = readFile()
+    private val data = readFile()
 
     fun processPart1(): Int =
         data
-            .sumOf { point -> point.neighbours()
-                .count { it !in data } }
+            .sumOf { point ->
+                point
+                    .neighbours()
+                    .count { it !in data }
+            }
 
-    fun processPart2(): Int =
-        0
+    fun processPart2(): Int {
+        val xLimits = data.minOf { it.x } - 1..data.maxOf { it.x } + 1
+        val yLimits = data.minOf { it.y } - 1..data.maxOf { it.y } + 1
+        val zLimits = data.minOf { it.z } - 1..data.maxOf { it.z } + 1
+
+        val toVisit = mutableListOf(Point3D(xLimits.first, yLimits.first, zLimits.first))
+        val visited = mutableSetOf<Point3D>()
+
+        var sides = 0
+        while (toVisit.isNotEmpty()) {
+            val current = toVisit.removeFirst()
+            if (current !in visited) {
+                current.neighbours()
+                    .filter { it.x in xLimits && it.y in yLimits && it.z in zLimits }
+                    .forEach { next -> if (next in data) sides++ else toVisit += next }
+                visited += current
+            }
+        }
+        return sides
+    }
 
     private fun readFile(): Set<Point3D> {
         val fileData = getResourceAsText(filename)
 
         return fileData
-            .map { it.split(",")
-                .map(String::toInt) }
+            .map { row ->
+                row
+                    .split(",")
+                    .map(String::toInt)
+            }
             .map { (x, y, z) -> Point3D(x, y, z) }
             .toSet()
     }
--- aoc18/src/test/kotlin/de/havox_design/aoc2022/day18/Day18Test.kt
+++ aoc18/src/test/kotlin/de/havox_design/aoc2022/day18/Day18Test.kt
@@ -33,7 +33,7 @@ class Day18Test {
         @JvmStatic
         private fun getDataForTestProcessPart2(): Stream<Arguments> =
             Stream.of(
-                Arguments.of("sample.txt", 0)
+                Arguments.of("sample.txt", 58)
             )
     }
 }
